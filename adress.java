703716629ed6d63b778cc508461af3ac161b64bd
ayush <aayush.2005sng@gmail.com>
2025-08-06 16:12:15 +0530

    Initial commit

---
+++ adress.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class adress{
+    public static void main(String[] args){
+        System.out.println("F-325 Alpha 2 Greater Noida, Uttar Pradesh 201310");
+        System.out.print("F-325 Alpha 2 Greater Noida, ");
+        System.out.print("Uttar Pradesh 201310");
+    }
+}
