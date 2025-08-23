9eb3508f4c0da0e410e17e8f2268626fad3b6c10
ayush <aayush.2005sng@gmail.com>
2025-08-23 10:42:45 +0530

    Initial commit

---
+++ .vscode/Size.java
@@ -0,0 +1,12 @@
+// write a jva program to find the size and range of all primitive data types
+// write a program to check even or odd without using % operator
+import java.util.*;
+public class Size {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        int c = a<<b;
+        System.out.println(c);
+    }
+    
+}
---
+++ .vscode/rightshift.java
@@ -0,0 +1,11 @@
+
+
+public class rightshift {
+    public static void main(String [] args){
+        byte a = 20;
+        byte b = 2;
+        int c = a>>b;
+        System.out.println(c);
+    }
+    
+}
