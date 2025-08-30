90b40f2693fff97c9a31e528b40a1693976c43bc
ayush <aayush.2005sng@gmail.com>
2025-08-30 14:31:26 +0530

    Initial commit

--- .vscode/Shift.java
+++ .vscode/Shift.java
@@ -1,8 +1,8 @@
 
 public class Shift {
     public static void main(String[] args){
-        int a = 64928;
-        int b = 8;
+        int a = 10;
+        int b = 1;
         int c = a<<b;
         System.out.println(c);
     }
--- .vscode/rightshift.java
+++ .vscode/rightshift.java
@@ -2,8 +2,8 @@
 
 public class rightshift {
     public static void main(String [] args){
-        byte a = 20;
-        byte b = 2;
+        byte a = 56;
+        byte b = 1;
         int c = a>>b;
         System.out.println(c);
     }
